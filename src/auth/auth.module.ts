a8f019e92a98cdf3b8a45b4eec85cc5f1e12e70a
gturricepa <gturri@cepasafedrive.com>
2024-10-15 16:10:12 -0300

    feat[auth]: implements Jwt login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
 })
 export class AuthModule {}
