fde728df1972df3a4df2f56be2f7ef224a909333
gturricepa <gturri@cepasafedrive.com>
2024-10-16 08:57:39 -0300

    feat[Swagger]: create endpoint documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CEPA ANALYTICS - END POINTS DOCUMENTATION')
+    .setDescription(
+      'This documentation details the available endpoints, including methods, paths, and descriptions of the functionalities offered.',
+    )
+    .setVersion('1.0')
+    .addTag('user')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'This field is unique on DB and using for querys',
+    example: 'jhondoe@exemple.com',
+  })
   @IsEmail()
   email: string;
-
+  @ApiProperty({
+    name: 'This field is not unique on DB and using for Identification char',
+    example: 'Jhon Doe',
+  })
   @IsNotEmpty()
   name: string;
-
+  @ApiProperty({
+    description: 'This field is save in a HashCode on DB and using for auth',
+    example:
+      'user input is 123465, on bd its save like s!!w323$2%#$@(#aaadWOIKAD2312321mnmmjhaio14563',
+  })
   @IsNotEmpty()
   password: string;
-
+  @ApiProperty({
+    description:
+      'This field is not unique on DB and using for setRoutes and indentification',
+    example: '01',
+  })
   @IsNotEmpty()
   corporation_id: number;
 }
